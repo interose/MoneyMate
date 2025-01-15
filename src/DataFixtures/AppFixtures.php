eaac6f3d8eeb53d99e3698d8697f3013188be7bf
Marcus <interose@gmail.com>
2025-01-15 19:05:17 +0100

    use faker to create demo/test data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,216 +5,126 @@ namespace App\DataFixtures;
 use App\Entity\Account;
 use App\Entity\Category;
 use App\Entity\CategoryGroup;
-use App\Entity\CurrentBalance;
 use App\Entity\Setting;
-use App\Entity\SplitTransaction;
 use App\Entity\SubAccount;
 use App\Entity\Transaction;
-use App\Lib\SettingsHandler;
+use App\Lib\Manager\SettingsManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
-    /**
-     * @var ObjectManager
-     */
-    private $manager;
+    private ObjectManager $manager;
+    private Generator $faker;
 
     public function load(ObjectManager $manager): void
     {
         $this->manager = $manager;
+        $this->faker = Factory::create();
 
+        $this->createBase();
+
+        $this->createCategories();
+
+        $this->createDebitTransactions();
+
+        $this->createCreditTransactions($this->faker->dateTimeBetween('first day of last month', 'last day of last month'));
+        $this->createCreditTransactions($this->faker->dateTimeBetween('first day of this month', 'last day of this month'));
+        $this->createCreditTransactions($this->faker->dateTimeBetween('first day of next month', 'last day of next month'));
+
+        $manager->flush();
+    }
+
+    private function createBase(): void
+    {
         $account = new Account();
         $account->setName('This is a Testaccount');
-        $account->setAccountHolder('Test');
-        $account->setIban('test');
-        $account->setBic('test');
-        $account->setBankCode('test');
-        $account->setUrl('test');
-
+        $account->setBic($this->faker->swiftBicNumber);
+        $account->setBankCode($this->faker->swiftBicNumber);
+        $account->setUrl($this->faker->url);
         $this->manager->persist($account);
         $this->manager->flush();
 
         $subaccount = new SubAccount();
         $subaccount->setAccount($account);
         $subaccount->setEnabled(true);
-        $subaccount->setIban('test');
-        $subaccount->setBic('test');
-        $subaccount->setAccountNumber('test');
-        $subaccount->setBlz('test');
+        $subaccount->setIban($this->faker->iban('DE'));
+        $subaccount->setAccountNumber($this->faker->creditCardNumber);
         $subaccount->setDescription('This is a Testdescription');
+        $this->setReference(SubAccount::class, $subaccount);
         $this->manager->persist($subaccount);
         $this->manager->flush();
 
-//        $settings = new Settings();
-//        $settings->setName(SettingsHandler::SETTING_MAIN_ACCOUNT);
-//        $settings->setValue((string) $subaccount->getId());
-//        $this->manager->persist($settings);
-//        $this->manager->flush();
-//        unset($settings);
-
-        $balance = new CurrentBalance();
-        $balance->setSubAccount($subaccount);
-        $balance->setBalance(500000);
-        $this->manager->persist($balance);
+        $setting = new Setting();
+        $setting->setName(SettingsManager::SETTING_MAIN_ACCOUNT);
+        $setting->setValue($subaccount->getId());
+        $this->manager->persist($setting);
         $this->manager->flush();
-
-        $cG = $this->createCategoryGroup('group-1');
-
-        $c = $this->createCategory('category-1-1', $cG);
-
-//        $settings = new Settings();
-//        $settings->setName(SettingsHandler::SETTING_LUXURY_CATEGORIES);
-//        $settings->setValue((string) $c->getId());
-//        $this->manager->persist($settings);
-//        $this->manager->flush();
-//        unset($settings);
-
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-08-02'), 15.99, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-08-25'), 30.65, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-07-31'), 13.19, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-09-01'), 62.59, $subaccount, $c);
-
-        $t = $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-06-16'), 110, $subaccount, $c);
-        $this->createSplitTransaction($t, \DateTime::createFromFormat('Y-m-d', '2020-06-10'), 10, $c);
-        $this->createSplitTransaction($t, \DateTime::createFromFormat('Y-m-d', '2020-06-04'), 26, $c);
-        $this->createSplitTransaction($t, \DateTime::createFromFormat('Y-m-d', '2020-06-09'), 54, $c);
-        $this->createSplitTransaction($t, \DateTime::createFromFormat('Y-m-d', '2020-05-28'), 13, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-06-11'), 67.59, $subaccount, $c);
-
-        $c = $this->createCategory('category-1-2', $cG);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-08-02'), 36.4, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-08-25'), 41.25, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-07-31'), 9.14, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-09-01'), 25.96, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-06-21'), 384.29, $subaccount, $c);
-
-        $cG = $this->createCategoryGroup('group-2');
-
-        $c = $this->createCategory('category-2-1', $cG);
-
-//        $settings = new Settings();
-//        $settings->setName(SettingsHandler::SETTING_SAVINGS_CATEGORIES);
-//        $settings->setValue((string) $c->getId());
-//        $this->manager->persist($settings);
-//        $this->manager->flush();
-//        unset($settings);
-
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-08-05'), 41.37, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-08-12'), 55.9, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-07-30'), 46.91, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-09-02'), 41.17, $subaccount, $c);
-
-        $c = $this->createCategory('category-2-2', $cG);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-08-05'), 6.1, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-08-12'), 28.33, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-07-30'), 25.19, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-09-02'), 54.09, $subaccount, $c);
-
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-08-11'), 31.8, $subaccount);
-
-        $c = $this->createCategory('category-3', null, true);
-
-//        $settings = new Settings();
-//        $settings->setName(SettingsHandler::SETTING_SALARY_CATEGORIES);
-//        $settings->setValue((string) $c->getId());
-//        $this->manager->persist($settings);
-//        $this->manager->flush();
-//        unset($settings);
-
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-08-08'), 7.02, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-08-23'), 4.96, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-07-18'), 9.44, $subaccount, $c);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-09-04'), 8.25, $subaccount, $c);
-
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-09-15'), 120, $subaccount, null, 'credit');
-
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-06-3'), 12.53, $subaccount);
-        $this->createTransaction(\DateTime::createFromFormat('Y-m-d', '2020-06-13'), 247.28, $subaccount, null, 'debit', 'MUENCHEN HOTEL XY\\MUENCHEN\\DE', 'test1234');
     }
 
-    /**
-     * @param string $name
-     *
-     * @return CategoryGroup
-     */
-    private function createCategoryGroup(string $name)
+    private function createCategories(): void
     {
-        $cG = new CategoryGroup();
-        $cG->setName($name);
-        $this->manager->persist($cG);
-        $this->manager->flush();
+        $this->createMany(CategoryGroup::class, 5, function (CategoryGroup $group, int $i) {
+            $group->setName(sprintf('Category Group %d', $i + 1));
+        });
 
-        return $cG;
+        $this->createMany(Category::class, 10, function (Category $category, int $i) {
+            $category->setName($this->faker->word());
+            $category->setCategoryGroup($this->getReference(CategoryGroup::class.'_'.($i + 1) % 3, CategoryGroup::class));
+        });
     }
 
-    /**
-     * @param string $name
-     * @param CategoryGroup|null $cG
-     *
-     * @return Category
-     */
-    private function createCategory(string $name, ?CategoryGroup $cG = null, ?bool $treeIgnore = false)
+    private function createDebitTransactions(): void
     {
-        $c = new Category();
-        $c->setName($name);
-        $c->setCategoryGroup($cG);
-        $c->setTreeIgnore($treeIgnore);
-        $this->manager->persist($c);
-        $this->manager->flush();
+        $this->createMany(Transaction::class, 100, function (Transaction $t, int $i) {
+            $t->setCategory($this->getReference(Category::class.'_'.($i + 1) % 10, Category::class));
 
-        return $c;
+            $dt = $this->faker->dateTimeBetween('first day of last month', 'last day of next month');
+            $t->setBookingDate($dt);
+            $t->setValutaDate($dt);
+            $t->setAmount(intval($this->faker->randomFloat(2, 10, 500) * 100));
+            $t->setCreditDebit('debit');
+            $t->setName($this->faker->company);
+            $t->setDescription($this->faker->text());
+            $t->setDescriptionRaw($this->faker->text());
+            $t->setBookingText('DIRECT DEBIT');
+            $t->setAccountNumber($this->faker->iban('DE'));
+            $t->setBankCode($this->faker->swiftBicNumber);
+            $t->setChecksum(md5($this->faker->text));
+            $t->setSubAccount($this->getReference(SubAccount::class, SubAccount::class));
+        });
     }
 
-    /**
-     * @param \DateTime     $d
-     * @param float         $amount
-     * @param Category|null $c
-     * @param SubAccount    $subAccount
-     * @param string        $creditDebit
-     * @param string|null   $name
-     * @param string|null   $description
-     *
-     * @return Transaction
-     */
-    private function createTransaction(\DateTime $d, float $amount, SubAccount $subAccount, ?Category $c = null, ?string $creditDebit = 'debit', ?string $name = null, ?string $description = null)
+    private function createCreditTransactions(\DateTime $dt): void
     {
         $t = new Transaction();
-        $t->setCategory($c);
-        $t->setBookingDate($d);
-        $t->setValutaDate($d);
-        $t->setAmount(intval($amount*100));
-        $t->setCreditDebit($creditDebit);
-        $t->setName($name);
-        $t->setDescription($description);
-        $t->setBookingText('LASTSCHRIFT');
-        $t->setAccountNumber('DE34302201900024938816');
-        $t->setChecksum(md5($d->format('Y-m-d').$d->format('Y-m-d').$t->getAmount().time()));
-        $t->setSubAccount($subAccount);
+        $t->setCategory($this->getReference(Category::class.'_1', Category::class));
+        $t->setBookingDate($dt);
+        $t->setValutaDate($dt);
+        $t->setAmount(intval($this->faker->randomFloat(2, 3000, 5000) * 100));
+        $t->setCreditDebit('credit');
+        $t->setName($this->faker->company);
+        $t->setDescription($this->faker->text());
+        $t->setDescriptionRaw($this->faker->text());
+        $t->setBookingText('DIRECT CREDIT');
+        $t->setAccountNumber($this->faker->iban('DE'));
+        $t->setBankCode($this->faker->swiftBicNumber);
+        $t->setChecksum(md5($this->faker->text));
+        $t->setSubAccount($this->getReference(SubAccount::class, SubAccount::class));
 
         $this->manager->persist($t);
-        $this->manager->flush();
-
-        return $t;
     }
 
-    /**
-     * @param Transaction   $t
-     * @param \DateTime     $d
-     * @param float         $amount
-     * @param Category|null $c
-     */
-    private function createSplitTransaction(Transaction $t, \DateTime $d, float $amount, Category $c = null)
+    private function createMany(string $className, int $count, callable $factory)
     {
-        $s = new SplitTransaction();
-        $s->setTransaction($t);
-        $s->setValutaDate($d);
-        $s->setAmount(intval($amount*100));
-        $s->setCategory($c);
-        $s->setDescription('Test');
-
-        $this->manager->persist($s);
-        $this->manager->flush();
+        for ($i = 0; $i < $count; ++$i) {
+            $entity = new $className();
+            $factory($entity, $i);
+            $this->manager->persist($entity);
+            // store for usage later as App\Entity\ClassName_#COUNT#
+            $this->addReference($className.'_'.$i, $entity);
+        }
     }
 }
