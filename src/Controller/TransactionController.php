ee6033fa58ccdbc4d589a34426bc96d4f0fa9ce7
Marcus Meirose <interose@gmail.com>
2026-02-19 16:22:08 +0100

    fixed typo

--- src/Controller/TransactionController.php
+++ src/Controller/TransactionController.php
@@ -64,14 +64,14 @@ class TransactionController extends AbstractController
     public function editCategory(TransactionRepository $transactionRepository, SplitTransactionRepository $splitTransactionRepository, EntityManagerInterface $entityManager, CategoryRepository $categoryRepository, Request $request): Response
     {
         $transaction = $transactionRepository->findOneById($request->request->getInt('transactionId'));
-        $catgory = $categoryRepository->findOneById($request->request->getInt('categoryId'));
+        $category = $categoryRepository->findOneById($request->request->getInt('categoryId'));
 
         $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
 
         if ($request->request->has('splitTransactionId') && 0 !== $request->request->getInt('splitTransactionId')) {
             $splitTransaction = $splitTransactionRepository->findOneById($request->request->getInt('splitTransactionId'));
 
-            $splitTransaction->setCategory($catgory);
+            $splitTransaction->setCategory($category);
             $entityManager->persist($transaction);
             $entityManager->flush();
 
@@ -82,7 +82,7 @@ class TransactionController extends AbstractController
                 'splitTransaction' => $splitTransaction,
             ]);
         } else {
-            $transaction->setCategory($catgory);
+            $transaction->setCategory($category);
             $entityManager->persist($transaction);
             $entityManager->flush();
 
