a3e8af3998ec5a238e20084f2acb27ec6a92ed79
Marcus Meirose <interose@gmail.com>
2026-04-07 10:35:51 +0200

    new ui for search input

--- assets/controllers/ta_filter_controller.js
+++ assets/controllers/ta_filter_controller.js
@@ -8,13 +8,14 @@ export default class extends Controller {
     static targets = [
         'form',
         'month', 'year', 'pickerLabel', 'pickerDropdown', 'pickerChevron', 'dropdownYearLabel', 'dropdownMonths',
-        'searchQuery'
+        'searchQuery', 'searchQueryReset', 'searchQueryShortcut'
     ];
 
     month = null;
     year = null;
     dropdownOpen = false;
     dropdownYear = null;
+    onOutsideClick = null
 
     // -------------------------
     // Lifecycle
@@ -22,41 +23,41 @@ export default class extends Controller {
     initialize() {
         this.debouncedSubmit = debounce(this.debouncedSubmit.bind(this), 500);
     }
-
     connect() {
         this.month = parseInt(this.monthTarget.value);
         this.year = parseInt(this.yearTarget.value);
+
+        this.onOutsideClick = this.#handleOutsideClick.bind(this)
+        document.addEventListener('click', this.onOutsideClick)
+    }
+    disconnect() {
+        document.removeEventListener('click', this.onOutsideClick)
     }
 
     // -------------------------
     // Month Picker Actions
     // -------------------------
-    prevMonth(e) {
+    prevMonth() {
         this.#monthStep(-1);
         this.#updatePickerLabel();
         this.#updateHiddenFields();
     }
-
-    nextMonth(e) {
+    nextMonth() {
         this.#monthStep(1);
         this.#updatePickerLabel();
         this.#updateHiddenFields();
     }
-
     pickerPrevYear() {
         this.dropdownYear -=1;
         this.#renderDropdown();
     }
-
     pickerNextYear() {
         this.dropdownYear +=1;
         this.#renderDropdown();
     }
-
     togglePickerDropdown() {
         this.dropdownOpen ? this.#closePickerDropdown() : this.#openPickerDropdown();
     }
-
     dropdownSelectMonth(el) {
         this.month = parseInt(el.target.dataset.value);
         this.year = this.dropdownYear;
@@ -78,12 +79,11 @@ export default class extends Controller {
     }
 
     debouncedSubmit() {
-        // this.searchQueryResetTarget.classList.remove('hidden');
-        // this.searchQueryShortcutTarget.classList.add('hidden');
+        this.searchQueryResetTarget.classList.remove('hidden');
+        this.searchQueryShortcutTarget.classList.add('hidden');
 
         this.formTarget.requestSubmit();
     }
-
 
     // -------------------------
     // Month Picker Private
@@ -93,16 +93,13 @@ export default class extends Controller {
         if (this.month > 12) { this.month = 1;  this.year++; }
         if (this.month < 1)  { this.month = 12; this.year--; }
     }
-
     #updatePickerLabel() {
         this.pickerLabelTarget.innerText = `${MONTHS[this.month - 1]} ${this.year}`;
     }
-
     #updateHiddenFields() {
         this.monthTarget.value = this.month;
         this.yearTarget.value = this.year;
     }
-
     #openPickerDropdown() {
         this.dropdownOpen  = true;
         this.dropdownYear = this.year
@@ -112,14 +109,12 @@ export default class extends Controller {
         this.pickerDropdownTarget.classList.remove('hidden');
         this.pickerChevronTarget.style.transform = 'rotate(180deg)';
     }
-
     #closePickerDropdown() {
         this.dropdownOpen  = false;
 
         this.pickerDropdownTarget.classList.add('hidden');
         this.pickerChevronTarget.style.transform = '';
     }
-
     #renderDropdown() {
         this.dropdownYearLabelTarget.innerText = this.dropdownYear;
 
@@ -134,6 +129,11 @@ export default class extends Controller {
                     btn.classList.add('text-white/25')
                 }
             });
+    }
+    #handleOutsideClick(event) {
+        if (!this.element.contains(event.target)) {
+            this.#closePickerDropdown();
+        }
     }
 
     // -------------------------
