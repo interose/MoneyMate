5aefac226b58bae751438bc4b0403ac1281f9855
Marcus <interose@gmail.com>
2026-04-16 23:12:19 +0200

    disabled not transformed modules

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -13,36 +13,38 @@ class DashboardController extends AbstractController
     #[Route('/', name: 'app_dashboard')]
     public function index(TransactionRepository $repository, SettingsManager $settingsManager): Response
     {
-        $mainAccount = $settingsManager->get(SettingsManager::SETTING_MAIN_ACCOUNT);
-        if ($mainAccount) {
+        return $this->redirectToRoute('app_transaction_index');
 
-            $start = new \DateTime();
-            $end = clone $start;
-            $start->setTime(0, 0, 0);
-            $start->modify('- 13 months');
-            $end->setTime(23, 59, 59);
-
-            $turnover = $repository->getTurnoverByMonth($mainAccount, $start, $end);
-            $chartData = [];
-            $categories = [];
-
-            array_walk($turnover, function ($item) use (&$chartData, &$categories) {
-                $categories[] = $item['month_name'];
-                $chartData['debit'][] = $item['debit'] / 100;
-                $chartData['credit'][] = $item['credit'] / 100;
-            });
-
-            $turnover = [
-                'categories' => $categories,
-                'chartData' => $chartData,
-            ];
-        } else {
-            $turnover = [];
-            $this->addFlash('error_static', 'Please set a main account in the settings.');
-        }
-
-        return $this->render('dashboard/index.html.twig', [
-            'turnoverChartData' => json_encode($turnover),
-        ]);
+//        $mainAccount = $settingsManager->get(SettingsManager::SETTING_MAIN_ACCOUNT);
+//        if ($mainAccount) {
+//
+//            $start = new \DateTime();
+//            $end = clone $start;
+//            $start->setTime(0, 0, 0);
+//            $start->modify('- 13 months');
+//            $end->setTime(23, 59, 59);
+//
+//            $turnover = $repository->getTurnoverByMonth($mainAccount, $start, $end);
+//            $chartData = [];
+//            $categories = [];
+//
+//            array_walk($turnover, function ($item) use (&$chartData, &$categories) {
+//                $categories[] = $item['month_name'];
+//                $chartData['debit'][] = $item['debit'] / 100;
+//                $chartData['credit'][] = $item['credit'] / 100;
+//            });
+//
+//            $turnover = [
+//                'categories' => $categories,
+//                'chartData' => $chartData,
+//            ];
+//        } else {
+//            $turnover = [];
+//            $this->addFlash('error_static', 'Please set a main account in the settings.');
+//        }
+//
+//        return $this->render('dashboard/index.html.twig', [
+//            'turnoverChartData' => json_encode($turnover),
+//        ]);
     }
 }
