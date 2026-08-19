605eddde41616139a9dc156e0141cb6bd7e0002a
Marcus Meirose <interose@gmail.com>
2026-08-19 09:21:13 +0200

    refactored settings area

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Form\SettingsType;
 use App\Lib\Manager\SettingsManager;
+use App\Repository\AccountRepository;
+use App\Service\EncryptionService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,7 +14,7 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/settings')]
 class SettingsController extends AbstractController
 {
-    #[Route('/', name: 'app_settings_global_index', methods: ['GET', 'POST'])]
+    #[Route('/', name: 'app_settings_index', methods: ['GET', 'POST'])]
     public function index(SettingsManager $settingsManager): Response
     {
         return $this->render('settings/index.html.twig', [
@@ -20,22 +22,30 @@ class SettingsController extends AbstractController
         ]);
     }
 
-    #[Route('/edit', name: 'app_settings_global_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, SettingsManager $settingsManager): Response
+    #[Route('/accounts', name: 'app_settings_accounts_index', methods: ['GET'])]
+    public function accounts(AccountRepository $repository, EncryptionService $encryptionService): Response
     {
-        $form = $this->createForm(SettingsType::class, $settingsManager->all());
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $settingsManager->setMany($form->getData());
-
-            $this->addFlash('success', 'Settings updated!');
-
-            return $this->redirectToRoute('app_settings_global_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('settings/edit.html.twig', [
-            'form' => $form,
+        return $this->render('settings/accounts.html.twig', [
+            'accounts' => $repository->findAll(),
         ]);
     }
+
+//    #[Route('/edit', name: 'app_settings_global_edit', methods: ['GET', 'POST'])]
+//    public function edit(Request $request, SettingsManager $settingsManager): Response
+//    {
+//        $form = $this->createForm(SettingsType::class, $settingsManager->all());
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $settingsManager->setMany($form->getData());
+//
+//            $this->addFlash('success', 'Settings updated!');
+//
+//            return $this->redirectToRoute('app_settings_global_index', [], Response::HTTP_SEE_OTHER);
+//        }
+//
+//        return $this->render('settings/edit.html.twig', [
+//            'form' => $form,
+//        ]);
+//    }
 }
